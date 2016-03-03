456ae46b6e59ca8bc8ff92666e58344d02fbb172
elliettej <elliettej@msn.com>
2016-03-03 12:35:41 -0800

    created llocal repository

---
+++ thurs.js
@@ -0,0 +1,19 @@
+/**
+ * Review
+ **/
+
+// Write a function `nameThatAnimal` that takes two arguments, a name, 
+// and a type of animal.
+
+	function nameThatAnimal (name, animal) {
+							// VARIABLES
+
+// The function should alert a sentence naming the animal.
+	alert('The ' + animal + ' is called ' + name + '.');
+	
+	}
+	
+// Call your function with a funny name and a real animal.
+
+	nameThatAnimal('Missy','cat');
+					// our values
