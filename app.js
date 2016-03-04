12712e25304f5d507f16b37d798a24a7570f0792
elliettej <elliettej@msn.com>
2016-03-04 09:56:56 -0800

    created function namePicker and variable to splice names from array

---
+++ app.js
@@ -0,0 +1,12 @@
+var codetalkNames = [];
+
+function resetNames (){
+	codetalkNames = ['Bernice', 'Elliette', 'Mirna', 'Erinn', 'Aimee', 'Yadira', 'Jessica', 'Janene', 'Denise'];
+
+}
+
+function namePicker(){
+	var namePick = codetalkNames.splice(Math.floor(Math.random() * codetalkNames.length), 1)
+		// splice this from codetalkNames array - taking 1 away
+		// rounding down, multiplying a random whole number, times the length of the array, spliced by 1
+}
