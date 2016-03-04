39b212570bb49b8996ae1d9d4f2f11bb3b6718ab
elliettej <elliettej@msn.com>
2016-03-04 10:22:35 -0800

    not too sure

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var codetalkNames = [];
 
 function resetNames (){
 	codetalkNames = ['Bernice', 'Elliette', 'Mirna', 'Erinn', 'Aimee', 'Yadira', 'Jessica', 'Janene', 'Denise'];
-
 }
 
 function namePicker(){
